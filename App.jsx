a678713bcbbb28e3bca106332c397caf689e8f0b
DakshAsati <149083569+DakshAsati@users.noreply.github.com>
2024-12-23 23:57:13 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Chai from "./Chai"
+
+function App() {
+  const username = "chai or code"
+
+  return (
+    <>  
+    <Chai/>
+    <h1>chai or react {username}</h1>
+</>
+  )
+}
+
+export default App
